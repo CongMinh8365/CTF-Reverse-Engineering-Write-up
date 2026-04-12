b5823ba6567ff49d772ee6edfe295ac5ab73de1d
CongMinh8365 <doancongminh69696969@gmail.com>
2026-04-12 23:43:04 +0700

    Update hook.js

--- UMassCTF2026/Lego Clicker/hook.js
+++ UMassCTF2026/Lego Clicker/hook.js
@@ -1,5 +1,5 @@
 Java.perform(function() {
-    console.log("[*] Dang trien khai Radar tim kiem ClassLoader...");
+    console.log("[*] Dang tim kiem ClassLoader...");
 
     var found = false;
     Java.enumerateClassLoaders({
@@ -10,16 +10,17 @@ Java.perform(function() {
                     Java.classFactory.loader = loader; 
                     found = true;
                     
-                    // CÚ ĐẤM XUYÊN THỦNG LAZY LOAD (Chuẩn Java 100%)
+                    // BYPASS LAZY LOAD
                     // Ép JVM nạp class và bắt buộc chạy khối static {} ngay lập tức
                     var Class = Java.use("java.lang.Class");
                     Class.forName("com.example.LegoClicker.SessionValidator", true, loader);
                     console.log("[+] Da ep chay khoi static, liblegocore.so DA VAO RAM!");
-
-                    // Lúc này file .so chắc chắn 100% đã có mặt
+ 
                     var targetModule = Process.findModuleByName("liblegocore.so");
                     if (targetModule) {
                         console.log("[+] Da tim thay liblegocore.so tai: " + targetModule.base);
+                        
+                        // Ép Anti-debug trả về 0
                         var antiDebugTarget = targetModule.base.add(0x21E80);
                         Interceptor.attach(antiDebugTarget, {
                             onLeave: function(retval) {
@@ -27,26 +28,26 @@ Java.perform(function() {
                                 retval.replace(ptr(0));
                             }
                         });
-
+                        
                         var SessionValidator = Java.use("com.example.LegoClicker.SessionValidator");
-                        console.log("[*] Buoc 3: Ép điểm Top 1 Sever và lấy cờ xịn...");
+                        console.log("[*] Ép điểm Top 1 và lấy Flag...");
                         
-                        var score = int64("1000000000000000"); // 15 số 0 (1 Triệu Tỷ)
-                        var magic_j8 = int64("4521136641424654587"); // Mã tương ứng
+                        var score = int64("1000000000000000"); 
+                        var magic_j2 = int64("4521136641424654587"); 
                         
-                        var flag = SessionValidator.syncBrickCache(score, magic_j8);
+                        var flag = SessionValidator.syncBrickCache(score, magic_j2);
                         console.log("\n==================================");
-                        console.log("[!] BINGO! CỜ XỊN TOP 1 CỦA ÔNG ĐÂY: " + flag);
+                        console.log("[!] FLAG: " + flag);
                         console.log("==================================\n");
                     } else {
-                        console.log("[-] Loi: Khong the tin duoc, van khong thay .so!");
+                        console.log("[-] ERROR");
                     }
                 }
             } catch (e) {}
         },
         onComplete: function() {
             if (!found) {
-                console.log("[-] Van chua thay class. Ong quay sang Nox BAM VAO NÚT LEADERBOARD di!");
+                console.log("[-] Van chua thay class");
             }
         }
     });
